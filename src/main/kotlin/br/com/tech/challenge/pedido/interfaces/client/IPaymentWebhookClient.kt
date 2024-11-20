54b88c54d5cc1757f540360da1c65b3703ae10e4
carla-gb <carla.paula@grupoboticario.com.br>
2024-11-20 17:49:37 -0300

    ajusta clients

--- src/main/kotlin/br/com/tech/challenge/pedido/interfaces/client/IPaymentWebhookClient.kt
+++ src/main/kotlin/br/com/tech/challenge/pedido/interfaces/client/IPaymentWebhookClient.kt
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
-//@FeignClient(url = "svc-payment", value = "payment")
-@FeignClient(url = "localhost:8081", value = "payment")
+@FeignClient(url = "svc-payment", value = "payment")
+//@FeignClient(url = "localhost:8081", value = "payment")
 interface IPaymentWebhookClient {
 
   @RequestMapping(method = [RequestMethod.POST], value = ["/payment/create/{id}"])
